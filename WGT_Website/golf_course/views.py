97f5e25a1c8cca73b42f026b519fd5f09391d59e
mkrul <mishakrul@gmail.com>
2019-03-18 06:45:39 -0400

    add urls, templates

--- WGT_Website/golf_course/views.py
+++ WGT_Website/golf_course/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import generic
+from .models import GolfCourse
+
+class GolfCourseListView(generic.ListView):
+    model = GolfCourse
+    template_name = 'golf_course/golf_course_list.html'
+    context_object_name = 'golf_courses'
--- WGT_Website/wgt_site/urls.py
+++ WGT_Website/wgt_site/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
+from golf_course import urls as golf_course_urls
+from . views import homepage
+
 urlpatterns = [
+    url('', homepage),
     url(r'^admin/', admin.site.urls),
+    url('golf_course/', include(golf_course_urls)),
 ]
 
 admin.site.site_header = 'Wake Golf Tour'
