59092efa4ee8af3ef505d70ca474c6f0b3710f97
mkrul <mishakrul@gmail.com>
2019-03-18 08:26:32 -0400

    add tournament details

--- WGT_Website/tournament/urls.py
+++ WGT_Website/tournament/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url('', views.TournamentListView.as_view(),
         name='tournament_list'),
+    url('<int:pk>/', views.TournamentDetailView.as_view(),
+        name='tournament_detail'),
 ]
