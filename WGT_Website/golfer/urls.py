4a2f724e2c481b9c9114383e9a0eabe8e7ef256c
mkrul <mishakrul@gmail.com>
2019-03-30 23:36:18 -0400

    add images and css

--- WGT_Website/golfer/urls.py
+++ WGT_Website/golfer/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [ 
     path('', views.GolferListView.as_view(),
         name='golfer_list'),
-    path('<int:pk>/',views.GolferDetailView.as_view(),name='golfer_detail')
+    path('<int:pk>/',views.GolferDetailView.as_view(),name='golfer_detail'),
+    path('<int:pk>/round_scores/', views.GolferRoundScoresView.as_view(),
+        name='golfer_round_scores')
 ]
 
