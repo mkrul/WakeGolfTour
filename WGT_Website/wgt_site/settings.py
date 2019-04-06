5190e38d9148cd5a64eeea8e856f118f72c19372
mkrul <mishakrul@gmail.com>
2019-04-06 19:18:12 -0400

    add golfer polls

--- WGT_Website/wgt_site/settings.py
+++ WGT_Website/wgt_site/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'golfer',
     'golf_course',
     'tournament',
+    'golfer_polls',
 ]
 
 MIDDLEWARE = [
--- WGT_Website/wgt_site/urls.py
+++ WGT_Website/wgt_site/urls.py
@@ -5,6 +5,7 @@ from . views import homepage
 from golf_course import urls as golf_course_urls
 from golfer import urls as golfer_urls
 from tournament import urls as tournament_urls
+from golfer_polls import urls as golfer_polls_urls
 
 urlpatterns = [
     path(r'^', homepage),
@@ -12,6 +13,7 @@ urlpatterns = [
     path('golf_course/', include(golf_course_urls)),
     path('golfer/', include(golfer_urls)),
     path('tournament/', include (tournament_urls)),
+    path('golfer_polls/', include(golfer_polls_urls)),
 ]
 
 admin.site.site_header = 'Wake Golf Tour'
